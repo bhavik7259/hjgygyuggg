df73c1eacd9150dd70792cbb04d01cadcd8f8c68
bhavik7259 <47849830+bhavik7259@users.noreply.github.com>
2024-09-21 12:13:23 -0400

    Add files via upload

---
+++ bhavik/addphotos/Cal.java
@@ -0,0 +1,12 @@
+class Calc{
+    public void add(int a,int b){
+        int x;
+        System.out.println(a+b);
+    }
+}
+public class Cal{
+    public static void main(String[] args) {
+        Calc c =new Calc();
+        c.add(2,3);
+    }
+}
